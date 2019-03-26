4f8d7af65c0f2f4eceef91b703ea7d24c3385de0
Franklin Barreto <franklin-barreto@hotmail.com>
2019-03-26 01:33:22 +0000

    initial routes

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,8 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'AcmeController@index');
+Route::get('/form', 'FormController@index');
+Route::get('/questions', 'QuestionController@index');
