5e1cac54f20996c423250793d8897b4d0a7eacc5
Franklin Barreto <franklin-barreto@hotmail.com>
2019-03-28 15:02:51 +0000

    method save answers

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Answer extends Model
 {
-    //
+    protected $fillable = ['answer', 'question_id'];
+    public $timestamps = false;
+
+    public static function saveAnswers(array $inputs)
+    {
+        $data = [];
+        foreach ($inputs as $key => $value) {
+            if (isset($key) && $key != '_token') {
+                $data[] = array('answer' => $value, 'question_id' => $key);
+                Answer::create(array('answer' => $value, 'question_id' => $key));
+            }
+        }
+    }
 }
